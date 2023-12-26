9acedd58627f2677c0c9760989d0839308d92ff4
orson88 <62896830+orson88@users.noreply.github.com>
2023-12-26 07:23:55 +0300

    Update testing.py

--- tests/testing.py
+++ tests/testing.py
@@ -4,23 +4,18 @@ from minio import Minio
 from minio_access import list_models, create_client
 
 def test_list_models(mocker):
-    # Create a mock Minio client
+    
     mock_client = MagicMock(spec=Minio)
     
-    # Mock the create_client function to return the mock client
     mocker.patch('minio_access.create_client', return_value=mock_client)
     
-    # Mock the list_objects method of the client
     mock_client.list_objects.return_value = [{'object_name': 'mlflow/artifact1'}, {'object_name': 'mlflow/artifact2'}]
     
-    # Mock the list_objects method called inside the function
     mock_client.list_objects.side_effect = [
         [{'object_name': 'mlflow/artifact1/model1'}, {'object_name': 'mlflow/artifact1/model2'}],
         [{'object_name': 'mlflow/artifact2/model3'}, {'object_name': 'mlflow/artifact2/model4'}]
     ]
     
-    # Call the function under test
     result = list_models()
     
-    # Assert the expected output
     assert result == ['mlflow__artifact1/model1', 'mlflow__artifact2/model3']
